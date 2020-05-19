378f484b30be4b59f3da4eeba9924dc0a1bae78d
贺柳铭 <heliuming@heliumingdeMacBook-Pro.local>
2020-05-19 15:41:26 +0800

    init

---
+++ dist/precache-manifest.61b3cd0eae60d84eddfd9a84441e49b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b274f13bb757957ae07",
+    "url": "/css/app.8d23bc70.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "68e77505cf2f05c9f4292afbe950cd60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adaee2d2531d54d9c687",
+    "url": "/js/about.98f3b201.js"
+  },
+  {
+    "revision": "5b274f13bb757957ae07",
+    "url": "/js/app.4ceddbd5.js"
+  },
+  {
+    "revision": "991702d3b398a4760df5",
+    "url": "/js/chunk-vendors.fd2fb3ba.js"
+  },
+  {
+    "revision": "9c12e2ddacd6fc48b51116de86911f7a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
